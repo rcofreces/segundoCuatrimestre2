4defb3ecb592840d68c611e71c0c6914c361db91
rcofreces <cofrecesrbles@tdf.edu.ar>
2022-11-29 22:25:29 -0300

    29/11/22

---
+++ clase21-11/ejercicio2/script.js
@@ -0,0 +1,66 @@
+let btn1 = document.querySelector('.btn-1');
+btn1.addEventListener('click', () => {
+    btn1.value = 1;
+});
+
+let btn2 = document.querySelector('.btn-2');
+btn2.value = 2;
+
+let btn3 = document.querySelector('.btn-3');
+btn3.value = 3;
+
+let btnSuma = document.querySelector('.btn-suma');
+btnSuma.value = '+';
+
+let btn4 = document.querySelector('.btn-4');
+btn4.value = 4;
+
+let btn5 = document.querySelector('.btn-5');
+btn5.value = 5;
+
+let btn6 = document.querySelector('.btn-6');
+btn6.value = 6;
+
+let btnResta = document.querySelector('.btn-resta');
+btnResta.value = '-';
+
+let btn7 = document.querySelector('.btn-7');
+btn7.value = 7;
+
+let btn8 = document.querySelector('.btn-8');
+btn8.value = 8;
+
+let btn9 = document.querySelector('.btn-9');
+btn9.value = 9;
+
+let btnMultiplicar = document.querySelector('.btn-multiplicar');
+btnMultiplicar.value = '*';
+
+let btn0 = document.querySelector('.btn-0');
+btn0.value = 0;
+
+let btnPunto = document.querySelector('.btn-punto');
+btnPunto.value = '.';
+
+let btnBorrar = document.querySelector('.btn-borrar');
+btnBorrar.value = '';
+
+let btnDividir = document.querySelector('.btn-dividir');
+btnDividir.value = '/';
+
+let btnIgual = document.querySelector('.btn-igual');
+btnIgual.value = '=';
+
+let visor = document.querySelector('.visor');
+
+function sumar()
+
+function restar()
+
+function multiplicar()
+
+function dividir()
+
+function clear()
+
+btnBorrar.addEventListener('click', clear);
