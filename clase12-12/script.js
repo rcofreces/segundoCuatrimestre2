59c80df249fdcf2fee401bc3aac6ec3896f1e34a
rcofreces <cofrecesrbles@tdf.edu.ar>
2022-12-12 21:54:04 -0300

    12/12/22

--- clase12-12/script.js
+++ clase12-12/script.js
@@ -1,25 +1,28 @@
-let inputName = document.getElementById("name");
-let inputSurname = document.getElementById("surname");
-let btn = document.getElementById("button");
-let tableBody = document.getElementById("tab");
+let inputName = document.getElementById('name');
+let inputSurname = document.getElementById('surname');
+let btn = document.querySelector('.btn');
+
+btn.addEventListener('click', addUser);
 
 function addUser() {
-    let tr = document.createElement("tr");
-    let tdName = document.createElement("td");
-    let tdSurname = document.createElement("td");
+    let tr = document.createElement('tr');
+    let tdName = document.createElement('td');
+    let tdSurname = document.createElement('td');
+
     tdName.innerText = inputName.value;
     tdSurname.innerText = inputSurname.value;
-    tableBody.appendChild(tr);
+
+    let tbody = document.getElementById('tab');
+    tbody.appendChild(tr);
     tr.appendChild(tdName);
     tr.appendChild(tdSurname);
 }
 
-btn.addEventListener("click", addUser);
-
-fetch('https://randomuser.me/api/?results=5')
+fetch('https://randomuser.me/api/?results=1')
     .then(response => response.json())
     .then(data => {
-        console.log(`Nombre: ${data.results[0].name.first} Apellido: ${data.results[0].name.last}`)
+        console.log(`Nombre: ${data.results[0].name.first} Apellido: ${data.results[0].name.last} Titulo: ${data.results[0].name.title}`)
+    
         inputName.value = data.results[0].name.first;
         inputSurname.value = data.results[0].name.last;
     })
